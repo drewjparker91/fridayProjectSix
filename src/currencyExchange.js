2499e2962a072309cbba0c77985d31ad43982240
drewparker <drewjparker91@gmail.com>
2020-09-11 11:51:58 -0700

    bug fixing in main.js

--- src/currencyExchange.js
+++ src/currencyExchange.js
@@ -1,13 +1,13 @@
 export async function callAPIExchange() {
   try {
-    let response = await fetch (`https://v6.exchangerate-api.com/v6/${process.evn.API_KEY}/latest/USD`)
-    let jsonifiedResponse
+    let response = await fetch (`https://v6.exchangerate-api.com/v6/${process.evn.API_KEY}/latest/USD`);
+    let jsonifiedResponse;
     if (response.ok && response.status === 200) {
       jsonifiedResponse = await response.json();
     } else {
       jsonifiedResponse = false;
     }
-    return jsonifiedResponse
+    return jsonifiedResponse;
   } catch(error) {
     return false;
   }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
+import {callAPIExchange} from "./currencyExchange.js";
 import $ from "jquery";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {callAPIExchange} from "./currencyExchange.js";
+
 
 
 async function retrieveRate(currency, usDollar) {
   const response = await callAPIExchange();
-  if (!reponse) {
+  if (!response) {
     $(".error").html("Sorry, an error has ocurred while trying to get your currency exchange");
   } else {
     giveExchangeRate(currency, usDollar, response);
@@ -14,23 +15,30 @@ async function retrieveRate(currency, usDollar) {
 }
 
 function giveExchangeRate(currency, usDollar, response) {
-  if (currency ===  AUD) {
-    $(".rateResult").html(`The exchange rate is ${response.conversion_rates.AUD} and your total is ${(response.conversion_rates.AUD*usDollar).toFixed(2)}`)
-  } else if (currency === CHF) {
-    $(".rateResult").html(`The exchange rate is ${response.conversion_rates.CHF} and your total is ${(response.conversion_rates.CHF*usDollar).toFixed(2)}`)
-  } else if (currency === DKK) {
-    $(".rateResult").html(`The exchange rate is ${response.conversion_rates.DKK} and your total is ${(response.conversion_rates.DKK*usDollar).toFixed(2)}`)
-  } else if (currency === ILS) {
-    $(".rateResult").html(`The exchange rate is ${response.conversion_rates.ILS} and your total is ${(response.conversion_rates.ILS*usDollar).toFixed(2)}`)
-  } else if (currency === NOK) {
-    $(".rateResult").html(`The exchange rate is ${response.conversion_rates.NOK} and your total is ${(response.conversion_rates.NOK*usDollar).toFixed(2)}`)
+  if (currency ===  "AUD") {
+    $(".rateResult").html(`The exchange rate is ${response.conversion_rates.AUD} and your total is ${(response.conversion_rates.AUD*usDollar).toFixed(2)}`);
+  } else if (currency === "CHF") {
+    $(".rateResult").html(`The exchange rate is ${response.conversion_rates.CHF} and your total is ${(response.conversion_rates.CHF*usDollar).toFixed(2)}`);
+  } else if (currency === "DKK") {
+    $(".rateResult").html(`The exchange rate is ${response.conversion_rates.DKK} and your total is ${(response.conversion_rates.DKK*usDollar).toFixed(2)}`);
+  } else if (currency === "ILS") {
+    $(".rateResult").html(`The exchange rate is ${response.conversion_rates.ILS} and your total is ${(response.conversion_rates.ILS*usDollar).toFixed(2)}`);
+  } else if (currency === "NOK") {
+    $(".rateResult").html(`The exchange rate is ${response.conversion_rates.NOK} and your total is ${(response.conversion_rates.NOK*usDollar).toFixed(2)}`);
   } else {
-    $(".rateResult").html ("Sorry, the conversion rate you have entered is not available at this time.")
+    $(".rateResult").html ("Sorry, the conversion rate you have entered is not available at this time.");
   }
 }
 
 
 $(document).ready(function () {
   $("#inputForm").submit(function (event) {
+    event.preventDefault();
+    let usDollar = parseInt($(".exchangeAmount").val());
+    let inputtedCurrency = $(".exchangeCurrency").val();
+    $(".exchangeAmount").val("");
+    $(".exchangeCurrency").val("");
+    let currency = inputtedCurrency.toUpperCase();
+    retrieveRate(currency, usDollar);
   });
 });    
