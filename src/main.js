b6681a6151373b5af44ce7b96d449e78b90ead27
drewparker <drewjparker91@gmail.com>
2020-09-11 16:33:26 -0700

    update currency symbols and error message

--- src/main.js
+++ src/main.js
@@ -7,24 +7,23 @@ import './css/styles.css';
 async function retrieveRate(currency, usDollar) {
   let response = await callAPIExchange();
   if (!response) {
-    console.log($(".error"))
     $(".error").html("Sorry, an error has ocurred while trying to get your currency exchange");  
-  } else {   
-    giveExchangeRate(currency, response, usDollar);    
+  } else {      
+    return giveExchangeRate(currency, response, usDollar);
   }
 }
 
 function giveExchangeRate(currency, response, usDollar) {
   if (currency ===  "AUD") {
-    $(".rateResult").html(`The exchange rate for the Australian Dollar is ${response.conversion_rates.AUD} and your converted total is ${(response.conversion_rates.AUD*usDollar).toFixed(2)}`);
+    $(".rateResult").html(`The exchange rate for the Australian Dollar is ${response.conversion_rates.AUD} and your converted total is $${(response.conversion_rates.AUD*usDollar).toFixed(2)}.`);
   } else if (currency === "CHF") {
-    $(".rateResult").html(`The exchange rate for the Swiss Franc is ${response.conversion_rates.CHF} and your converted total is ${(response.conversion_rates.CHF*usDollar).toFixed(2)}`);
+    $(".rateResult").html(`The exchange rate for the Swiss Franc is ${response.conversion_rates.CHF} and your converted total is ${(response.conversion_rates.CHF*usDollar).toFixed(2)} SFr.`);
   } else if (currency === "DKK") {
-    $(".rateResult").html(`The exchange rate for the Danish Krone is ${response.conversion_rates.DKK} and your converted total is ${(response.conversion_rates.DKK*usDollar).toFixed(2)}`);
+    $(".rateResult").html(`The exchange rate for the Danish Krone is ${response.conversion_rates.DKK} and your converted total is ${(response.conversion_rates.DKK*usDollar).toFixed(2)} Kr.`);
   } else if (currency === "ILS") {
-    $(".rateResult").html(`The exchange rate for the Israeli New Shekel is ${response.conversion_rates.ILS} and your converted total is ${(response.conversion_rates.ILS*usDollar).toFixed(2)}`);
+    $(".rateResult").html(`The exchange rate for the Israeli New Shekel is ${response.conversion_rates.ILS} and your converted total is ₪${(response.conversion_rates.ILS*usDollar).toFixed(2)}.`);
   } else if (currency === "NOK") {
-    $(".rateResult").html(`The exchange rate for the Norwegian Krone is ${response.conversion_rates.NOK} and your converted total is ${(response.conversion_rates.NOK*usDollar).toFixed(2)}`);
+    $(".rateResult").html(`The exchange rate for the Norwegian Krone is ${response.conversion_rates.NOK} and your converted total is ${(response.conversion_rates.NOK*usDollar).toFixed(2)} kr.`);
   } else {
     $(".rateResult").html ("Sorry, the conversion rate you have entered is not available at this time.");
   }
